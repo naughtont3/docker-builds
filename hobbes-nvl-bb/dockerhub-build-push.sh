5d1d375423fe0af0000df1489fcdd195dd78362f
Thomas Naughton <naughtont@ornl.gov>
2016-12-06 11:28:52 -0500

    add 'skip_push' flag to top of build/push scripts

--- hobbes-nvl-bb/dockerhub-build-push.sh
+++ hobbes-nvl-bb/dockerhub-build-push.sh
@@ -3,6 +3,10 @@
 username=naughtont3
 imagename=hobbes-nvl-bb
 
+# Skip final "docker push" 1=skip, 0=push
+skip_push=0
+#skip_push=1
+
 # End Configs
 #####################################
 
@@ -27,7 +31,12 @@ if [ "${basename}" != "${imagename}" ] ; then
 fi
 
 docker build -t="${username}/${imagename}" .  || die "Docker Build failed"
-docker push ${username}/${imagename}          || die "Docker Push failed"
+
+if [ 0 == ${skip_push} ] ; then
+    docker push ${username}/${imagename}      || die "Docker Push failed"
+else
+    echo "SKIPPING 'docker push'"
+fi
 
 echo "*** Build/Push of ${username}/${imagename} Finished! ***"
 exit 0
--- ubuntu1404qemu/dockerhub-build-push.sh
+++ ubuntu1404qemu/dockerhub-build-push.sh
@@ -3,6 +3,10 @@
 username=naughtont3
 imagename=ubuntu1404qemu
 
+# Skip final "docker push" 1=skip, 0=push
+skip_push=0
+#skip_push=1
+
 # End Configs
 #####################################
 
@@ -27,7 +31,13 @@ if [ "${basename}" != "${imagename}" ] ; then
 fi
 
 docker build -t="${username}/${imagename}" .  || die "Docker Build failed"
-docker push ${username}/${imagename}          || die "Docker Push failed"
+
+if [ 0 == ${skip_push} ] ; then
+    docker push ${username}/${imagename}      || die "Docker Push failed"
+else
+    echo "SKIPPING 'docker push'"
+fi
+
 
 echo "*** Build/Push of ${username}/${imagename} Finished! ***"
 exit 0
