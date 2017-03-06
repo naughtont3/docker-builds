1277d2d32a10a5c19f7495193ab7c2b572f83684
Thomas Naughton <naughtont@ornl.gov>
2017-03-06 09:45:12 -0500

    hpcc-mpi: update startSSHD helper script
    NOTE: I added the rundir to Dockerfile so that step is not
    needed anymore for this container and can likely be a one liner now.

--- hpcc-mpi/scripts/startSSHDs_v2.sh
+++ hpcc-mpi/scripts/startSSHDs_v2.sh
@@ -7,6 +7,6 @@ ve_name=`docker ps --filter name=$service_name --format='{{.Names}}'`
 
 #docker cp /tmp/setupSSH.sh $ve_name:/tmp
 echo "CMD: docker exec --user=root $ve_name /usr/sbin/sshd -p 2222"
-docker exec --user=root $ve_name mkdir /var/run/sshd
+docker exec --user=root $ve_name mkdir -p /var/run/sshd
 docker exec --user=root $ve_name /usr/sbin/sshd -p 2222
 
