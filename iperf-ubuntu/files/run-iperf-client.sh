7cf5a2bbaeae5072028413cf9e6069021806f666
Thomas Naughton <naughtont@ornl.gov>
2017-05-04 15:30:24 -0400

    iperf: update run-iperf-client/server.sh scripts

--- iperf-ubuntu/files/run-iperf-client.sh
+++ iperf-ubuntu/files/run-iperf-client.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-export PATH=/benchmarks/src/IPERF:$PATH
+#export PATH=/benchmarks/src/IPERF:$PATH
+export PATH=/benchmarks/src/local/bin:$PATH
 
 # IP Address where IPERF server runs
-SERVER_IPADDR=10.255.1.149
+#SERVER_IPADDR=10.255.1.149
+SERVER_IPADDR=10.0.2.3
 
 # Positive integer
 NUM_LOOPS=10
@@ -11,7 +13,16 @@ NUM_LOOPS=10
 # Time to sleep between loops
 SLEEPTIME=1
 
+LOGFILE=/tmp/2017.05.04-iperf-client-10G-tcp.log
+IPERF_OPTIONS="-n 10G"
+
 #-------
+
+if [ ! -x /usr/bin/seq ] ; then
+   echo "ERROR: missing '/usr/bin/seq' executable"
+   exit 1
+fi
+
 
 # Show interfaces
 if [ -x /sbin/ifconfig ] ; then
@@ -21,17 +32,18 @@ if [ -x /sbin/ifconfig ] ; then
 fi
 
 
-for cnt in {1..$NUM_LOOPS} ; do 
+for cnt in `/usr/bin/seq 1  $NUM_LOOPS` ; do 
 
     echo "# INFO: === $cnt ===" 
 
-    # Client (TCP)
-    iperf3 -T $cnt -c $SERVER_IPADDR 
-
-    # Client (UDP)
-    #iperf3 -T $cnt -c $SERVER_IPADDR -b10G
-
-    echo "# INFO: === Sleep $SLEEPTIME ==="
-    sleep $SLEEPTIME
+     # Client (TCP)
+     echo "CMD: iperf3 -A 0 -fg -T $cnt $IPERF_OPTIONS --logfile $LOGFILE -c $SERVER_IPADDR"
+     iperf3 -A 0 -fg -T $cnt $IPERF_OPTIONS --logfile $LOGFILE -c $SERVER_IPADDR 
+ 
+     # Client (UDP)
+     #iperf3 -A 0 -fg -T $cnt $IPERF_OPTIONS --logfile $LOGFILE -c $SERVER_IPADDR -b10G
+ 
+     echo "# INFO: === Sleep $SLEEPTIME ==="
+     sleep $SLEEPTIME
 
 done
--- iperf-ubuntu/files/run-iperf-server.sh
+++ iperf-ubuntu/files/run-iperf-server.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-export PATH=/benchmarks/src/IPERF:$PATH
+#export PATH=/benchmarks/src/IPERF:$PATH
+export PATH=/benchmarks/src/local/bin:$PATH
 
-SERVER_IPADDR=10.255.1.149
+#SERVER_IPADDR=10.255.1.149
+SERVER_IPADDR=10.0.2.3
 
-iperf3 -s -B $SERVER_IPADDR
+LOGFILE=/tmp/2017.05.04-iperf-server.log
+IPERF_OPTIONS=
+
+iperf3 -A 0 -fg $IPERF_OPTIONS --logfile $LOGFILE -s -B $SERVER_IPADDR
 
